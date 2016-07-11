e25c9d61eea28f0af832d7fba27f7b50021f561f
smalin <smallailin@gmail.com>
2016-07-11 17:31:16 +0800

    更新服务端基础配置，加入 meteorhacks:async 模块。

--- server/methods/wechat.js
+++ server/methods/wechat.js
@@ -1,8 +1,8 @@
-var config = {
-  appId: 'wxe6426b4eea2e32d8',
-  secret: '0aae97b9d3fbc6e6215e71e7b7158f43',
-  host: 'http://www.muyingpai.cn/'
-};
+/**
+ * 媛育微信授权登录配置
+ * @type {Object}
+ */
+var config = WechatConfig;
 Meteor.methods({
   /**
    * Login to Meteor with a Facebook access token
@@ -12,20 +12,33 @@ Meteor.methods({
    * @param accessToken Your Facebook access token
    * @returns {*}
    */
-  wechatLoginWithAccessToken: function (query) {
-    // check(accessToken, String);
-    // // check(email, Match.OneOf(String, null));
-    // check(openid, String);
-    // check(info, Object);
+  wechatLoginWithAccessToken: function(query) {
+    check(query, Object);
     console.log('req.query', query);
     var response = getTokenResponse(query);
     console.log('response', response);
-    var info = getIdentity(response.access_token, response.openid);
+    // 是否获取用户 openid，access_token 成功
+    if(!response.openid || !response.access_token){
+      throw new Meteor.Error(403, '获取用户 access_token 失败 !');
+    }
+    var info = getUserInfo(response.openid, response.openid);
+    console.log('info', info);
+
+    // 用户信息
+    if(info.error || !info.result){
+      throw new Meteor.Error(403, '获取用户信息失败 !');
+    }
+
+    info = info.result;
+    // 是否已关注该公众号
+    if(info.subscribe == 0){
+      throw new Meteor.Error(401, '未关注公众号 !');
+    }
     console.log('info', info);
 
     var openid = response.openid;
     var accessToken = response.access_token;
-    
+
     // email = email || "-" + id + "@facebook.com";
     var options, serviceData;
     serviceData = {
@@ -34,58 +47,44 @@ Meteor.methods({
     };
     options = {
       profile: {
-        wechat :info
+        wechat: info
       }
     };
 
-    // // Confirm that your accessToken is you
-    // try {
-    //   var tokenInfo = $FB('debug_token', {
-    //     input_token: accessToken,
-    //     access_token: Meteor.settings.facebook.appId + '|' + Meteor.settings.facebook.secret
-    //   });
-    // } catch (e) {
-    //   throw new Meteor.Error(500, 'Facebook login failed. An API error occurred.');
-    // }
-    //
-    // if (!tokenInfo.data.is_valid) {
-    //   throw new Meteor.Error(503, 'This access token is not valid.');
-    // }
-    //
-    // if (tokenInfo.data.app_id !== Meteor.settings.facebook.appId) {
-    //   throw new Meteor.Error(503, 'This token is not for this app.');
-    // }
-    //
-    // // Force the user id to be the access token's user id
-    // id = tokenInfo.data.user_id;
-
-    // Returns a token you can use to login
+    // 创建或更新用户
     var loginResult = Accounts.updateOrCreateUserFromExternalService('wechat', serviceData, options);
-    Meteor.users.update({_id: loginResult.userId}, {$set: {'profile.wechat': info}});
+    Meteor.users.update({
+      _id: loginResult.userId
+    }, {
+      $set: {
+        'profile.wechat': info
+      }
+    });
 
-    // Login the user
-    // Meteor.connection.setUserId(loginResult.userId);
+    // 生成登录用户token
     var stampedLoginToken = Accounts._generateStampedLoginToken();
     Accounts._insertLoginToken(loginResult.userId, stampedLoginToken);
     return stampedLoginToken;
-    
-    // Return the token and the user id
-    return loginResult;
   }
 });
 
+/**
+ * 加载用户 openid access_token
+ * @param  {[type]} query [description]
+ * @return {[type]}       [description]
+ */
 function getTokenResponse(query) {
   var response;
   try {
     response = HTTP.get(
-        "https://api.weixin.qq.com/sns/oauth2/access_token", {
-          params: {
-            code: query.code,
-            appid: config.appId,
-            secret: config.secret,
-            grant_type: 'authorization_code'
-          }
-        });
+      "https://api.weixin.qq.com/sns/oauth2/access_token", {
+        params: {
+          code: query.code,
+          appid: config.appId,
+          secret: config.secret,
+          grant_type: 'authorization_code'
+        }
+      });
 
     if (response.error) // if the http response was an error
       throw response.error;
@@ -94,24 +93,31 @@ function getTokenResponse(query) {
     if (response.content.error)
       throw response.content;
   } catch (err) {
-    throw _.extend(new Error("Failed to complete OAuth handshake with Wechat. " + err.message),
-        {response: err.response});
+    throw _.extend(new Error("Failed to complete OAuth handshake with Wechat. " + err.message), {
+      response: err.response
+    });
   }
 
   return response.content;
 }
 
+/**
+ * 通过网页授权接口获取用户信息
+ * @param  {[type]} access_token [description]
+ * @param  {[type]} openid       [description]
+ * @return {[type]}              [description]
+ */
 function getIdentity(access_token, openid) {
   console.log('getIdentity', 'accessToken:', access_token, 'openid', openid);
   try {
     var response = HTTP.get(
-        "https://api.weixin.qq.com/sns/userinfo", {
-          params: {
-            access_token: access_token,
-            openid: openid,
-            lang: 'zh_CN'
-          }
-        });
+      "https://api.weixin.qq.com/sns/userinfo", {
+        params: {
+          access_token: access_token,
+          openid: openid,
+          lang: 'zh_CN'
+        }
+      });
 
     if (response.error) // if the http response was an error
       throw response.error;
@@ -127,4 +133,22 @@ function getIdentity(access_token, openid) {
   }
 
   return response.content;
+}
+
+/**
+ * 通过获取用户基本信息接口获取用户基本信息
+ * 使用 async 模块等待数据
+ * @param  {[type]} openid [description]
+ * @return {[type]}        [description]
+ */
+function getUserInfo(openid) {
+  var response = Async.runSync(function(done) {
+
+    WeApi.getUser(openid, function(error, result) {
+      done(error, result);
+    });
+
+  });
+
+  return response;
 }
--- server/publications.js
+++ server/publications.js
@@ -1,27 +1,20 @@
-Meteor.publish('mamapas', function() {
-  return MamaPas.find();
-});
-
-Meteor.publish('signups', function() {
-  return Signups.find();
-});
-
 Meteor.publish('images', function() {
   return Images.find();
 });
 
-Meteor.publish('usersData', function() {
-  if (this.userId) {
-    return Meteor.users.find();
-  } else {
+Meteor.publish('activity', function() {
+  return Activity.find();
+});
+
+Meteor.publish('prize', function() {
+  return PrizeList.find();
+});
+
+// 只取该用户自己的奖品
+Meteor.publish('userPrizes', function() {
+  if(this.userId){
+    return UserPrizesList.find({userId: this.userId});
+  }else{
     this.ready();
   }
-});
-
-Meteor.publish('gifts', function () {
-  return Gifts.find();
-});
-
-Meteor.publish('coupons', function () {
-  return Coupons.find();
 });
