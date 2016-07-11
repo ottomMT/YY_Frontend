fb953e1e942c42a8f89865fdc4160c5351ffa349
程伟 <chengweibeyond@qq.com>
2016-07-11 12:34:23 +0800

    添加「分享增加摇奶瓶机会」界面

--- client/applications/shake_bottle/shake_bottle.js
+++ client/applications/shake_bottle/shake_bottle.js
@@ -24,6 +24,21 @@ Template.shakeBottle.events({
         },200);
         // $(".modal").css('display','none').find(".center-square").addClass("zoom");
     },
+    'click #continue-shake': function () {
+        // 隐藏摇奶瓶结果界面
+        $("#shake-result-modal .center-square").addClass("zoom");
+        setTimeout(function () {
+            $("#shake-result-modal").css('display','none');
+        },200);
+        // 显示分享提示
+        setTimeout(function () {
+            $("#share-modal").css('display','block');
+            setTimeout(function () {
+                $("#share-modal .center-square").removeClass("zoom");
+            },10);
+        },100);
+        return false;
+    }
     // 'click .center-square': function () {
     //     return false;
     // },
